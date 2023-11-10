120e9b16bc060a965d9c667d00532ba813602110
italochagas <felipe.italo@hotmail.com>
2023-11-09 23:29:57 -0300

    chore: axios setup made

---
+++ src/http/index.ts
@@ -0,0 +1,21 @@
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
+
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL: 'http://www.omdbapi.com/',
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => config,
+  (error) => Promise.reject(error),
+);
+
+axiosInstance.interceptors.response.use(
+  (response: AxiosResponse) => response,
+  (error) => Promise.reject(error),
+);
+
+export default axiosInstance;
