9be05587ed4849c73a87553313f626c9c4f1d806
italochagas <felipe.italo@hotmail.com>
2023-11-10 20:54:25 -0300

    feat: created getters and mutations

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,22 @@
 import Vue from 'vue';
 import axiosInstance from '@/http';
 import Vuex from 'vuex';
-import { Movie } from '@/components/types';
+import { Movie } from '@/types';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store<{
+  errorFetchingData: boolean;
   favoriteMovies: Movie[];
   isLoading: boolean;
-  selectedMovie: Movie | null
+  selectedMovie: Movie | null;
+  isAlertVisible: boolean;
 }>({
   state: {
+    errorFetchingData: false,
     favoriteMovies: [],
     isLoading: false,
+    isAlertVisible: false,
     selectedMovie: null,
   },
   getters: {
@@ -22,12 +26,20 @@ export default new Vuex.Store<{
     getSelectedMovie(state) {
       return state.selectedMovie;
     },
+    getFavoriteMovies(state) {
+      return state.favoriteMovies;
+    },
+    getIsAlertVisible(state) {
+      return state.isAlertVisible;
+    },
+    getErrorFetchingData(state) {
+      return state.errorFetchingData;
+    },
   },
   mutations: {
     setFavoriteMovies(state, payload) {
-      console.log('payload', payload);
-      console.log('state', state);
-      state.favoriteMovies = { ...state.favoriteMovies, ...payload };
+      state.favoriteMovies = [...state.favoriteMovies, payload];
+      state.isAlertVisible = true;
     },
     setLoading(state, payload) {
       state.isLoading = payload;
@@ -35,22 +47,38 @@ export default new Vuex.Store<{
     setSelectedMovie(state, payload) {
       state.selectedMovie = payload;
     },
+    setAlertVisibility(state, isVisible) {
+      state.isAlertVisible = isVisible;
+    },
+    setSelectedMovieByImdbId(state, imdbId) {
+      state.selectedMovie = state.favoriteMovies.find((movie) => movie.imdbID === imdbId) ?? null;
+    },
+    setErrorFetchingData(state, payload) {
+      state.errorFetchingData = payload;
+    },
   },
   actions: {
     async fetchData({ commit }, value) {
-      console.log('Valor recebido', value);
-      console.log('ENVS', process.env);
       commit('setLoading', true);
+      commit('setErrorFetchingData', false);
 
       try {
-        const response = await axiosInstance.get(
+        const response = await axiosInstance.get<Movie>(
           `?t=${value.movieName}&apikey=${process.env.VUE_APP_API_URL_OMDB_API_KEY}`,
         );
         commit('setSelectedMovie', response.data);
         commit('setLoading', false);
+        if (response.data.Error) {
+          commit('setErrorFetchingData', true);
+        }
       } catch (error) {
-        console.error('Erro ao buscar dados:', error);
+        commit('setErrorFetchingData', true);
       }
+    },
+    hideAlert({ commit }) {
+      setTimeout(() => {
+        commit('setAlertVisibility', false);
+      }, 3000);
     },
   },
   modules: {},
