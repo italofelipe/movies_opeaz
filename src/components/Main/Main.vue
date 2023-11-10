70f3eb0b9ee17533930ec789e0f066453a2dd03a
italochagas <felipe.italo@hotmail.com>
2023-11-09 23:33:11 -0300

    feat: main section and movie info component created

---
+++ src/components/Main/Main.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-main>
+    <div v-if="isLoading">
+      <v-progress-circular indeterminate color="primary"></v-progress-circular>
+    </div>
+    <v-row>
+      <v-col cols="10">
+        <div v-if="selectedMovie">
+          <movie-info :selectedMovie="selectedMovie" />
+        </div>
+      </v-col>
+    </v-row>
+  </v-main>
+</template>
+
+<script lang="ts">
+import { mapGetters } from 'vuex';
+import MovieInfo from '../MovieInfo/MovieInfo.vue';
+
+export default {
+  name: 'MainSection',
+  components: {
+    MovieInfo,
+  },
+  computed: {
+    ...mapGetters({ isLoading: 'getIsLoading', selectedMovie: 'getSelectedMovie' }),
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/MovieInfo/MovieInfo.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="movie-info-container">
+    <div>
+      <v-img :src="selectedMovie?.Poster" />
+    </div>
+    <div>
+      <h3>{{ selectedMovie?.Title }}</h3>
+      <p class="bold">{{ selectedMovie?.Year }}</p>
+      <p class="font-weight-regular">{{ selectedMovie?.Plot }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { PropType } from 'vue';
+import { Movie } from '../types';
+
+export default {
+  name: 'MovieInfoComponent',
+  props: {
+    selectedMovie: Object as PropType<Movie>,
+  },
+};
+</script>
+
+<style>
+.movie-info-container {
+  display: flex;
+  flex-direction: row;
+  height: 450px;
+  width: 100%;
+}
+</style>
