db1c69a21340ef9e364b649e4b13cb883b7a6cde
italochagas <felipe.italo@hotmail.com>
2023-11-10 21:01:54 -0300

    test: test file renamed

---
+++ tests/unit/Favorites.spec.ts
@@ -0,0 +1,38 @@
+import { mount, createLocalVue } from '@vue/test-utils';
+import Vuex, { Store } from 'vuex';
+import Favorites from '@/components/Favorites/Favorites.vue';
+import { Movie } from '@/types';
+import favoriteMovies from '../mocks/mock';
+
+const localVue = createLocalVue();
+
+localVue.use(Vuex);
+describe('Favorites.vue', () => {
+  let wrapper;
+  let store: Store<{
+    errorFetchingData: boolean;
+    favoriteMovies: Movie[];
+    isLoading: boolean;
+    selectedMovie: Movie | null;
+    isAlertVisible: boolean;
+  }>;
+
+  beforeEach(() => {
+    store = new Vuex.Store({
+      getters: {
+        getFavoriteMovies: () => favoriteMovies,
+      },
+      mutations: {
+        setSelectedMovieByImdbId: jest.fn(),
+      },
+    });
+  });
+
+  it('renders the component', () => {
+    wrapper = mount(Favorites, {
+      store,
+      localVue,
+    });
+    expect(wrapper.exists()).toBe(true);
+  });
+});
