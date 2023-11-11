bcaf00a0ce53be2d6c86865a86ab74ff112e2da6
italochagas <felipe.italo@hotmail.com>
2023-11-10 21:01:32 -0300

    feat: added new font family and grid structure remade

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,19 @@
     <v-app>
       <v-container fluid class="pl-0 pr-0">
         <v-row>
-          <v-col cols="2">
-            <Favorites />
-          </v-col>
-          <v-col cols="10">
+          <v-col cols="12" >
             <Header />
             <MainSection />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-divider class="mt-5"></v-divider>
+        </v-row>
+        <v-row justify="center">
+          <v-col cols="4">
+            <Favorites v-if="favoriteMovies.length > 0" />
+
+            <h2 v-else>To see your favorite movies, search in the input above</h2>
           </v-col>
         </v-row>
       </v-container>
@@ -18,6 +25,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { mapGetters } from 'vuex';
 import Header from './components/Header/Header.vue';
 import Favorites from './components/Favorites/Favorites.vue';
 import MainSection from './components/Main/Main.vue';
@@ -28,7 +36,14 @@ export default defineComponent({
     Favorites,
     MainSection,
   },
+  computed: {
+    ...mapGetters({ favoriteMovies: 'getFavoriteMovies' }),
+  },
 });
 </script>
 
-<style></style>
+<style>
+* {
+  font-family: 'Quicksand', sans-serif;
+}
+</style>
