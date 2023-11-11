b5c073eebeab05b6620a4e86f8ef4ef4b47f11c1
italochagas <felipe.italo@hotmail.com>
2023-11-10 21:00:40 -0300

    feat: alert component implemented and conditional rendering logic made

--- src/components/Main/Main.vue
+++ src/components/Main/Main.vue
@@ -3,9 +3,14 @@
     <div v-if="isLoading">
       <v-progress-circular indeterminate color="primary"></v-progress-circular>
     </div>
-    <v-row>
+    <v-alert v-if="isAlertVisible" dense type="success">Added to bookmarks!</v-alert>
+    <v-row align="center" justify="center">
       <v-col cols="10">
-        <div v-if="selectedMovie">
+        <div v-if="errorFetchingData">
+          <h3>Oops, looks like your movie went to Azkhaban! What about trying another one?</h3>
+        </div>
+
+        <div v-else-if="selectedMovie">
           <movie-info :selectedMovie="selectedMovie" />
         </div>
       </v-col>
@@ -23,7 +28,12 @@ export default {
     MovieInfo,
   },
   computed: {
-    ...mapGetters({ isLoading: 'getIsLoading', selectedMovie: 'getSelectedMovie' }),
+    ...mapGetters({
+      isLoading: 'getIsLoading',
+      selectedMovie: 'getSelectedMovie',
+      isAlertVisible: 'getIsAlertVisible',
+      errorFetchingData: 'getErrorFetchingData',
+    }),
   },
 };
 </script>
